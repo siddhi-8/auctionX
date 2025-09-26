0b92421ed574379a107f1255b7f1a5105e8067f3
Sidds <162460065+siddhi-8@users.noreply.github.com>
2025-09-27 00:40:13 +0530

    Update drizzle.config.ts
    modified drizzle.config.ts

---
+++ my-app/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+});
+// he first commit madhe empty hita atta bghu ya
