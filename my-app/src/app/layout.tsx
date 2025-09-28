9accdcfa2a4b76c27c3265e882d7598704ee5268
Sidds <162460065+siddhi-8@users.noreply.github.com>
2025-09-28 21:33:21 +0530

    bring in shadcn and revalidate after adding items

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter as FontSans, Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" }); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
       >
         {children}
       </body>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,21 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { database } from "@/db/Database";
 import { bids as bidsSchema } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
 // Make the function async
 export default async function HomePage() {
   const bids = await database.query.bids.findMany();
 
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto py-12">
       <form
         action={async (formData: FormData) => {
           "use server";
           // const bid = formData.get("bid") as string;
           await database.insert(bidsSchema).values({});
+          revalidatePath("/");
         }}
       >
-        <input name="bid" placeholder="Bid" />
-        <button type="submit">Place Bid</button>
+        <Input name="bid" placeholder="Bid" />
+        <Button type="submit">Place Bid</Button>
       </form>
 
         {bids.map((bid) => (
