e366762680fac978df54c5207375c079cc89d848
Abhinav Chavan <149501379+XeNoBaNg@users.noreply.github.com>
2026-05-02 18:33:28 +0530

    Update next.config.ts

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -13,5 +13,13 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
